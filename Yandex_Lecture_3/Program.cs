8bfc219a287d6e15a265e6b81a66d8752c54d88d
Alexandr <a.a.shapovov@bk.ru>
2023-05-24 13:17:36 +0300

    CS

---
+++ Yandex_Lecture_3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Yandex_Lecture_3
+{
+    internal class Program //Множество
+    {
+        const int setSize = 10;
+
+       
+        static void Main(string[] args)
+        {
+
+            
+        }
+    }
+}
